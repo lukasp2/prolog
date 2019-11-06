33c1edc9fd55b54c998d236a008bd923f1967d95
lukasp2 <45101415+lukasp2@users.noreply.github.com>
2019-11-06 15:01:27 +0800

    Add files via upload

---
+++ main.pl
@@ -0,0 +1,95 @@
+% Game: 10 questions, the program thinks of something, the user tries to
+% figure out what the program is thinking about by asking
+% questions. The questions are on the format has(X) or is(Y).
+
+% program function flow
+%                                                  MAX 10 LOOPS
+%                                                   <---<---<--
+%                                                  |           |
+% start() --> begin() --> randomize_answer() --> input() --> has()
+%                                                  |
+%  ---------------<-------------<--------------<---
+% |
+% |             -> correct()  -                           -NO-> begin()
+% |           /                 \                       |
+%  -> is() ->                     -> next_game(), played 5 games?
+%             \                 /                       |
+%               -> incorrect() -                          -YES-> done.
+%
+
+consult('variables.pl') .
+consult('functions.pl') .
+
+% call this function to start, this function initiates the counters, and
+% prints startup message
+start() :-
+    write("Welcome to the game '10 questions'! In this game, I will think of something,"),
+    nl(),
+    write("and you will try to figure out what I am thinking about by asking me"),
+    nl(),
+    write("questions. You can ask 10 yes or no questions, then you have to guess what"),
+    nl(),
+    write("I'm thinking about. I'll make it easy on you, I'll let you choose if the"),
+    nl(),
+    write("answer is going to be a sport, an animal, or a celebrity."),
+    nl(), nl(),
+    write("Instructions:"), nl(),
+    write("Ask yes and no questions with the has() function, and guess the answer with"),
+    nl(),
+    write("the is() function. You can try to guess the answer before asking all 10"),
+    nl(),
+    write("questions, but be careful, if the guess is wrong you lose!"), nl(), nl(),
+    write("You can use the all_options(X) function if you want to display all"), nl(),
+    write("attributes that belongs to a category."), nl(), nl(),
+    write("Let's play!"),
+    nl(), nl(),
+
+    % initiates game counter
+    retractall(games_counter(_)),
+    assertz(games_counter(0)),
+    incr_games_counter(),
+
+    % initiate correct answers counter
+    retractall(corr_answers_counter(_)),
+    assertz(corr_answers_counter(0)),
+
+    % begins game
+    choose_category()
+ .
+
+% call this function to display answer. (debug tool)
+show_answer() :-
+    answer(Ans),
+    nth0(0, Ans, Idx0),
+    nth0(1, Ans, Idx1),
+    write("The answer is "), write(Idx0), nl(),
+    write("with attributes: "), write(Idx1)
+    .
+
+% display all options, this is a prototype. It displays duplicates.
+all_options(X) :-
+    unique(AllOptions),
+    (X==celebs -> nth0(0, AllOptions, CategoryOptions), print(CategoryOptions)
+     ;
+     X==animals -> nth0(1, AllOptions, CategoryOptions), print(CategoryOptions)
+     ;
+     X==sports -> nth0(2, AllOptions, CategoryOptions), print(CategoryOptions)
+     ;
+     write("Usage: all_options(X), X = {celebs, animals, sports}.")
+    )
+    .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables.pl
@@ -0,0 +1,55 @@
+% :- dynamic counter/1.
+
+answer([]) .
+:- dynamic answer/1 .
+
+
+
+% The categories contains two elements: what it is and what it has
+% What it has is contained in the sublist.
+% ex. sports = (is, [has,has,...,has])
+celebs(
+[
+    [ "Michael Jacksson",     [ "good moves", "one glove", "hit songs", "mad skills"]],
+    [ "Barrack Obama",        [ "a political carrer", "a wife"]],
+    [ "Adam Sandler",         [ "been in a movie", "no acting skills", "a wife"]],
+    [ "Slash",                [ "played guitar", "a top hat", "mad skills"]],
+    [ "Jackie Chan",          [ "been in a movie", "done kung fu"]]
+]).
+
+animals(
+[
+    [ turtle,     [ legs(4), teeth(molar), diet(plants), tail, shell ]],
+    [ mutant,     [ legs(3), teeth(canine), diet(pills), tail ]],
+    [ dog,        [ legs(4), teeth(molar), diet(meat), tail ]],
+    [ cat,        [ legs(4), teeth(canine), diet(meat), tail ]],
+    [ ostrich,    [ legs(2), teeth(none), diet(plants), tail ]],
+    [ human,      [ legs(2), teeth(hybrid), diet(hybrid) ]],
+    [ tiger,      [ legs(4), teeth(canine), diet(meat), tail ]]
+]).
+
+sports(
+[
+    [ tennis,     [ball, racket, net, sweat, running]],
+    [ basketball, [ball, sweat, net, goal, running]],
+    [ bowling,    [ball]],
+    [ hockey,     [club, puck, net, goal, ice]],
+    [ badminton,  [ball, net]],
+    [ chess,      [mindgame]],
+    [ waterpolo,  [water, ball]],
+    [ diving,     [pool, singles, jump]]
+]).
+
+unique(
+[
+    ["good moves", "one glove", "hit songs", "mad skills", "a political carrer",
+     "a wife", "been in a movie", "no acting skills", "played guitar", "a top hat",
+     "mad skills", "been in a movie", "done kung fu"],
+
+    [legs(4), legs(2), legs(3), teeth(molar), teeth(canine), teeth(hybrid),
+     teeth(none), diet(meat), diet(plants), diet(pills), diet(hybrid), tail, shell],
+
+    [ball, racket, net, sweat, running, goal, club, puck, ice, mindgame,
+     water, pool, singles, jump]
+]
+).
